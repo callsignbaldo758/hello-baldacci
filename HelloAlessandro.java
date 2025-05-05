52e363e7831068d6f15b75eebbcb3da42881d21d
callsignbaldo758 <alessandro.baldacci@capellinisauro.edu.it>
2025-05-05 07:05:57 +0000

    commit stampa 3

--- HelloAlessandro.java
+++ HelloAlessandro.java
@@ -2,5 +2,7 @@ import java.io.*;
 public class HelloAlessandro{
     public static void main(String[] args){
         System.out.println("hello baldo");
+        System.out.println("hello baldo");
+        System.out.println("hello baldo");
     }
 }
