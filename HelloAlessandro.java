6985616141c088397a14eda7280cc802ee192d57
callsignbaldo758 <alessandro.baldacci@capellinisauro.edu.it>
2025-05-12 06:33:02 +0000

    aggiunta con stampa stampa

--- HelloAlessandro.java
+++ HelloAlessandro.java
@@ -2,9 +2,19 @@ import java.io.*;
 public class HelloAlessandro{
     public static void main(String[] args){
         System.out.println("hello baldo");
-        System.out.println("hello baldo");
-        System.out.println("hello baldo");
-        System.out.println("24/08/2007");
-        System.out.println("forza milan");
+        Vector<Charater> lettere = new Vector<Character>();
+        lettere.add('a');
+        lettere.add('l');
+        lettere.add('e');
+        lettere.add('s');
+        lettere.add('s');
+        lettere.add('a');
+        lettere.add('n');
+        lettere.add('d');
+        lettere.add('r');
+        lettere.add('o');
+        for(int i = 0; i < lettere.size(); i++){
+            System.out.println(lettere.get(i));
+        }
     }
 }
